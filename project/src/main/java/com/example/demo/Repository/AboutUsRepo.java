66519ae528bec17394e1e8bdcd93bddda5da309b
Sanyajo <sakhatskiy2016@mail.ru>
2024-04-06 20:10:24 +0300

    Not annotated method overrides method annotated with @NonNullApi - игнорим

--- project/src/main/java/com/example/demo/Repository/AboutUsRepo.java
+++ project/src/main/java/com/example/demo/Repository/AboutUsRepo.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-
 public interface AboutUsRepo extends JpaRepository<AboutUs, Long> {
     List<AboutUs> findAll();
+
 }
