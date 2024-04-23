34f66ffb93604e2d8dbe1d9af8ff78a375b0fefb
Sanyajo <sakhatskiy2016@mail.ru>
2024-04-23 14:46:24 +0300

    Added an annotation

--- project/src/main/java/com/example/demo/Configurations/SecurityConfig.java
+++ project/src/main/java/com/example/demo/Configurations/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.demo.Service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final CustomUserDetailsService userDetailsService;
 
