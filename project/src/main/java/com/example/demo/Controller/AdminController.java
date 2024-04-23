5d72a1a9953bad888aad3257d6bbe687b452ce3e
Sanyajo <sakhatskiy2016@mail.ru>
2024-04-23 14:46:14 +0300

    Added an annotation

---
+++ project/src/main/java/com/example/demo/Controller/AdminController.java
@@ -0,0 +1,42 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Models.User;
+import com.example.demo.Models.enums.Role;
+import com.example.demo.Service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+public class AdminController {
+    private final UserService userService;
+
+    @GetMapping("/admin")
+    public String admin(Model model, Principal principal) {
+        model.addAttribute("users", userService.list());
+        model.addAttribute("user", userService.getUserByPrincipal(principal));
+        return "admin";
+    }
+
+    @PostMapping("/admin/user/ban/{id}")
+    public String userBan(@PathVariable("id") Long id) {
+        userService.banUser(id);
+        return "redirect:/admin";
+    }
+
+    @PostMapping("/admin/user/edit")
+    public String userEdit(@RequestParam("userId") User user, @RequestParam Map<String, String> form) {
+        userService.changeUserRoles(user, form);
+        return "redirect:/admin";
+    }
+}
